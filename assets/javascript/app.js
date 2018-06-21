5417d4388b99b25a6eca45692553d41dc40a47ed
Daniel Potts <drp777@hotmail.com>
2018-06-21 13:40:22 -0500

    homework

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+/*
+1. Create Variables:
+var userChoice
+var questions
+var options
+
+2. create 30 second timer to initiate the game and end game at the end of the timer(use simple timer exercise)
+
+3. Create a new $(<div>) to hold 8 questions. 
+
+4. Create if/else statements to determine whether questions are right or wrong
+
+5. create counters to see if answers are correct.
+
+6. create a formula to determine what percentage someone got with conditions above 70% means ok but study up, over 80% is great job, over 90% is excellent
+*/
+
+
+
+//lets create our variables//
+var userChoice;
+var questions;
+var options;
+var initiateTimer; 
+
+
+
+//lets create the 30 second timer//
+setTimeout(thirtySeconds, 30000);
